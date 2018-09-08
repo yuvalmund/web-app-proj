c26bd3bbec1976cdb5f0f2c4af59fe279bbbb93b
RoniPaiss <shironi47@gmail.com>
2018-09-08 12:50:41 +0300

    ML finished

---
+++ RentAHouse/ML/ApartmentData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentAHouse.ML
+{
+    public class ApartmentData
+    {
+        [Column("0")]
+        public int cityID;
+
+        [Column("1")]
+        public float cityAvarageSalary;
+
+        [Column("2")]
+        public int region;
+
+        [Column("3")]
+        public float cityGraduatesPercent;
+
+        [Column("4")]
+        public int RoomsNumber;
+
+        [Column("5")]
+        public int sizeInMeters;
+
+        [Column("5")]
+        public bool isThereElivator;
+
+        [Column("6")]
+        public bool isThereParkingPlace;
+
+        [Column("7")]
+        public bool isRenovated;
+
+        [Column("8")]
+        public int price;
+    }
+}
