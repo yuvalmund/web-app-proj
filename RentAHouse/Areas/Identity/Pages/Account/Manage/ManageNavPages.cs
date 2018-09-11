5184619c0d7918966a5546ab65a2057ca129c479
yuvalmund <yuvalmund@gmail.com>
2018-09-11 20:03:22 +0300

    start Owner Apatments

--- RentAHouse/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ RentAHouse/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -20,6 +20,8 @@ namespace RentAHouse.Areas.Identity.Pages.Account.Manage
 
         public static string PersonalData => "PersonalData";
 
+        public static string ViewMyApatments => "ViewMyApatments";
+
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
@@ -33,6 +35,8 @@ namespace RentAHouse.Areas.Identity.Pages.Account.Manage
         public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
 
         public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
+
+        public static string ViewMyApatmentsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ViewMyApatments);
 
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
     
---
+++ RentAHouse/Areas/Identity/Pages/Account/Manage/MyApatements.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RentAHouse.Controllers;
+using RentAHouse.Data;
+using RentAHouse.Models;
+using Newtonsoft.Json;
+
+namespace RentAHouse.Areas.Identity.Pages.Account.Manage
+{
+ 
+
+    public class MyApatementsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
