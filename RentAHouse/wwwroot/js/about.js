37627c19c21dab171e111ac389b309638b67322d
yuvalmund <yuvalmund@gmail.com>
2018-10-14 16:13:18 +0300

    comments

--- RentAHouse/wwwroot/js/about.js
+++ RentAHouse/wwwroot/js/about.js
@@ -1,15 +1,19 @@
-﻿var svg = d3.select("#graphPie"),
+﻿// adding the graph and attributes
+var svg = d3.select("#graphPie"),
     width = +svg.attr("width"),
     height = +svg.attr("height"),
     radius = Math.min(width, height) / 2,
     g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+// creating a color array
 var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
+// sorting the values
 var pie = d3.pie()
     .sort(null)
     .value(function (d) { return d.count; });
 
+// creating the radius
 var path = d3.arc()
     .outerRadius(radius - 10)
     .innerRadius(0);
@@ -18,6 +22,7 @@ var label = d3.arc()
     .outerRadius(radius - 40)
     .innerRadius(radius - 40);
 
+// getting the data
 d3.csv("../../../../data/graphPie.csv", function (d) {
     d.count = +d.count;
     return d;
@@ -29,6 +34,7 @@ d3.csv("../../../../data/graphPie.csv", function (d) {
         .enter().append("g")
         .attr("class", "arc");
 
+    // filling the graph
     arc.append("path")
         .attr("d", path)
         .attr("fill", function (d) { return color(d.data.cityName); });
